9bcb0a6f36e4f27fc8edd5a82420be7680837913
Julian Irmler <julian.irmler@gmx.at>
2024-02-01 09:54:03 +0100

    Klasse BMICalculator

---
+++ JavaProgrammieren/JAVA_Hello_World/Konfigurationsmanagement/src/BMICalculatorTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void getBodyHeight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBodyWeight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateBMI() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void caluclateBMICategory() {
+    }
+}
