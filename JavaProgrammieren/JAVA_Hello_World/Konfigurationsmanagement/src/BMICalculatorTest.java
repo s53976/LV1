f6bb0087220488d3745a7f9cd81f40622c2006b6
Julian Irmler <julian.irmler@gmx.at>
2024-02-01 10:17:38 +0100

    Klasse BMICalculator

--- JavaProgrammieren/JAVA_Hello_World/Konfigurationsmanagement/src/BMICalculatorTest.java
+++ JavaProgrammieren/JAVA_Hello_World/Konfigurationsmanagement/src/BMICalculatorTest.java
@@ -1,20 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BMICalculatorTest {
 
     @org.junit.jupiter.api.Test
     void getBodyHeight() {
+        BMICalculator BMICalculator = new BMICalculator("Julian", "Irmler", 12, 12.5, 'M');
+        Assertions.assertEquals(2,BMICalculator.bodyHeight);
+        Assertions.assertEquals(3, BMICalculator.bodyWeight);
     }
 
     @org.junit.jupiter.api.Test
     void getBodyWeight() {
+        BMICalculator BMICalculator = new BMICalculator("Julian", "Irmler", 12, 12.5, 'M');
+        Assertions.assertEquals(5, BMICalculator.bodyWeight);
+        Assertions.assertEquals(4, BMICalculator.bodyHeight);
     }
 
-    @org.junit.jupiter.api.Test
+
+    @Test
+    void testGetBodyHeight() {
+        double erwartet = 24.777;
+        BMICalculator a = new BMICalculator("Julia", "Test", 170, 70.0, 'W');
+        a.calculateBMI();
+
+        Assertions.assertEquals(a,erwartet);
+    }
+
+    @Test
+    void testGetBodyWeight() {
+    }
+
+    @Test
     void calculateBMI() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void caluclateBMICategory() {
     }
 }
