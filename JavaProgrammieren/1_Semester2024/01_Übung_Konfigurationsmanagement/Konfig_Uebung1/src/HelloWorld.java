6133149c35f7b4124681053257cbb75457773ca3
Julian Irmler <julian.irmler@gmx.at>
2024-09-13 17:39:00 +0200

    HelloWorld

--- JavaProgrammieren/1_Semester2024/01_Übung_Konfigurationsmanagement/Konfig_Uebung1/src/HelloWorld.java
+++ JavaProgrammieren/1_Semester2024/01_Übung_Konfigurationsmanagement/Konfig_Uebung1/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World Konfigurationsmanagement");
         System.out.println("Test");
+        System.out.println("Test");
     }
 }
