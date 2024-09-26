7551920e555d86f2f227baee272d51ab0721b201
Julian Irmler <julian.irmler@gmx.at>
2024-09-26 15:59:20 +0200

    Calulator - Teil 1

---
+++ Java Programming/LV1/LV1/src/MyFirstClass.java
@@ -0,0 +1,10 @@
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("MyFirstClass");
+        System.out.println("Ich werde in der Console angezeigt");
+
+
+    }
+}
