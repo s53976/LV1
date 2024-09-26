cb762dbfc64f3d384a9e8ec1ad79ae93ea7c8e6f
Julian Irmler <julian.irmler@gmx.at>
2024-09-26 14:12:55 +0200

    B2 - IF Anweisung

---
+++ Java Programming/HelloWelt/HelloWorld/src/B2.java
@@ -0,0 +1,12 @@
+public class B2 {
+    public static void main(String[] args) {
+
+        int i = 102;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+
+
+    }
+}
