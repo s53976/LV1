e8c73230af15298effb4464b870306992b55d029
Julian Irmler <julian.irmler@gmx.at>
2024-09-26 14:12:12 +0200

    B1 - Verschachtelte IF Anweisung

---
+++ Java Programming/HelloWelt/HelloWorld/src/B1.java
@@ -0,0 +1,20 @@
+public class B1 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int sum = 0;
+        while (i <= 100) {
+            if (i%3 == 0) {
+                System.out.println(i);
+
+            }
+            //System.out.println(i);
+            i++;
+            //sum += i;
+        }
+       // System.out.println("Summe: "+ sum);
+
+
+    }
+
+}
