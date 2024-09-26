05d8bfdb02c866504d0550eb13b35d51fe64e37c
Julian Irmler <julian.irmler@gmx.at>
2024-09-26 15:58:58 +0200

    Calulator - Teil 1

---
+++ Java Programming/LV1/LV1/src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        String Name = "Rechner";
+        int a = 2;
+        int b = 5;
+        int ergebnis = 0;
+
+        ergebnis = a + b;
+
+        System.out.println(Name + " Ergebnis " + ergebnis);
+
+
+
+
+    }
+}
